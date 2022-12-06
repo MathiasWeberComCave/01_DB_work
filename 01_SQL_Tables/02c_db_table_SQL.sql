c0603618062a0f1913bc2aaf7a28157ce767f71a
MathiasWeberComCave <q-mwb@gmx.de>
2022-12-06 14:14:38 +0000

    Eindeutige Datensätze mit einem Primärschlüssel

--- 01_SQL_Tables/02c_db_table_SQL.sql
+++ 01_SQL_Tables/02c_db_table_SQL.sql
@@ -1,4 +1,4 @@
-/* Strukturen Unique - keine doppelten Datensätze mit UNIQUE*/
+/* Strukturen Unique - keine doppelten Datensätze mit Primary Key*/
 
 /* Datenbanken anzeigen */
 #SHOW DATABASES;
@@ -21,9 +21,11 @@ USE first;
 /* Tabelle und Datenbank in gewählter Datenbank anlegen falls nicht vorhanden */
 CREATE TABLE IF NOT EXISTS test
 (
-    /* Textvaraible mit 20 Zeichen und Ganzahl mit 3 Ziffern anlegen die nicht Null sind bzw. Default-Werte beinhalten */
-    name VARCHAR(20) UNIQUE NOT NULL DEFAULT "TBA",
-    age INT(3) UNIQUE NOT NULL DEFAULT 0
+    /* Textvaraible mit 20 Zeichen und Ganzahl mit 3 Ziffern anlegen die nicht Null sind bzw. Default-Werte beinhalten.
+    Dazu ein Primärchlüssel zur eindeutigen Kennzeichnung eines Datensatzes.*/
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) DEFAULT "TBA",
+    age INT(3) DEFAULT 0
 );
 
 /* Tabellen anzeigen */
@@ -38,7 +40,7 @@ DESCRIBE test;
 /* Daten eingeben */
 INSERT INTO test(name,age) VALUES ("Grizabella",29);
 INSERT INTO test(age,name) VALUES (35,"Alonzo");
-INSERT INTO test() VALUES ();
+INSERT INTO test() VALUES();
 
 /* Inhalte der Tabelle anzeigen */
 SELECT * FROM test;
