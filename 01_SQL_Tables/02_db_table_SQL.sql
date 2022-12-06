97b8b7d3192f7ca286fb6fff0d0fd1185c371a79
MathiasWeberComCave <q-mwb@gmx.de>
2022-12-06 08:30:15 +0000

    Arbeitsstrukturen sind angelegt

---
+++ 01_SQL_Tables/02_db_table_SQL.sql
@@ -0,0 +1,14 @@
+/* Strukturen*/
+
+/* Datenbanken anzeigen*/
+SHOW DATABASES;
+
+/* Datenbanken löschen falls vorhanden*/
+DROP DATABASE IF EXISTS first;
+
+/* Datenbanken anzeigen*/
+SHOW DATABASES;
+
+/* Datenbanken anlegen falls nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS first;
+
